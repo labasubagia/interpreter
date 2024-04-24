1600be0388cab5749247c234d08120f51b9aec01
Laba Subagia <labasubagia22@gmail.com>
2024-04-24 14:50:01 +0800

    feat: parse return

--- parser/parser.go
+++ parser/parser.go
@@ -51,6 +51,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -67,6 +69,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectToken(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: We're skipping the expression until we encounter semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
 
 	// TODO: We're skipping the expression until we encounter semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
