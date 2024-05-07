35efee2fb2e7f83ce84a56ba81ba6a1992d801de
Laba Subagia <labasubagia22@gmail.com>
2024-05-07 15:21:33 +0800

    feat: binding & the environment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/labasubagia/interpreter/evaluator"
 	"github.com/labasubagia/interpreter/lexer"
+	"github.com/labasubagia/interpreter/object"
 	"github.com/labasubagia/interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
