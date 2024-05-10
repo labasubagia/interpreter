9bffd9008519ae515139cfbe14e84a5b86bfc612
Laba Subagia <labasubagia22@gmail.com>
2024-05-10 23:30:28 +0800

    feat: use release binary

--- interactive/main.py
+++ interactive/main.py
@@ -1,23 +1,36 @@
 import streamlit as st
 import subprocess
+import urllib.request 
 import os
 
+st.header("INTERPRETER")
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
+bin_path = os.path.join(dir_path, "./interpreter")
 
 if 'result' not in st.session_state:
     st.session_state['result'] = ''
 
+def download_bin():
+    if os.path.exists(bin_path):
+        return    
+    bin_url = "https://github.com/labasubagia/interpreter/releases/download/latest/interpreter"
+    with st.spinner(text="Download binary file"):
+        urllib.request.urlretrieve(bin_url, bin_path)
+        os.chmod(bin_path, 755)
 
 def handle_run():
-    bin_path = os.path.join(dir_path, "./interpreter")
+    if not os.path.exists(bin_path):
+        return
     args = (bin_path, "string", code)
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
-    if popen.stdout:
-        val = popen.stdout.read().decode('utf-8')
-        st.session_state['result'] = val if val else '~ No output'
+    if not popen.stdout:
+        return
+    val = popen.stdout.read().decode('utf-8')
+    st.session_state['result'] = val if val else '~ No output'
 
-st.header("INTERPRETER")
+download_bin()
 
 example_dir = os.path.join(dir_path,'../example')
 example_file = st.selectbox('Select Example', ['none'] + os.listdir(example_dir))
